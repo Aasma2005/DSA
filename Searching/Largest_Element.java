b6aa8283bc9e0d26e5c1ee6d94c0fe2f673bb0d4
Aasma2005 <aasmashaikh48237@gmail.com>
2026-03-06 11:08:03 +0530

    Add files via upload

---
+++ Searching/Largest_Element.java
@@ -0,0 +1,20 @@
+class Lrgest_Element{
+    public static void main(String[] args) {
+        int arr[]=new int[]{10,20,0};
+            int temp=0;
+        for(int i=0;i<arr.length;i++){
+        
+            for(int j=i+1;j<arr.length;j++){
+                
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+                
+            }
+          
+        }
+          System.out.println(temp);
+    }
+}
