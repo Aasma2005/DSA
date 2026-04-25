d35c42065fb0fd5750977972aa66739d345ebce1
Aasma2005 <aasmashaikh48237@gmail.com>
2026-04-25 09:54:19 +0530

    Add files via upload

---
+++ 15_IMP_DSA_ques(Array_String)/FindMin_Max_Element.java
@@ -0,0 +1,19 @@
+public class FindMin_Max_Element {
+    public static void main(String[] args) {
+        int[] arr=new int[]{3,2,1,56,10000,167};
+        int min=arr[0];
+        int max=arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            
+        }
+        System.out.println("min"+ " "+ min + "max"+ " "+max);
+    }
+    
+}
